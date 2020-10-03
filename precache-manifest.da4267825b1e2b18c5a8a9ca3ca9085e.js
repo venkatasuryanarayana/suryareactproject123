12b4b160f07f60f72ff6be16a12464e0ca0d2b97
venkatasuryanarayana <venkatasuryanarayana.n@apssdc.in>
2020-10-03 13:16:08 +0530

    Updates

---
+++ precache-manifest.da4267825b1e2b18c5a8a9ca3ca9085e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c36e5432b4c9268bb1b2680fe1aa6a",
+    "url": "/suryareactproject123/index.html"
+  },
+  {
+    "revision": "ced34de4a4dc5b7ad28a",
+    "url": "/suryareactproject123/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "769c53c8660ad56fd3f2",
+    "url": "/suryareactproject123/static/js/2.15fc217b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/suryareactproject123/static/js/2.15fc217b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ced34de4a4dc5b7ad28a",
+    "url": "/suryareactproject123/static/js/main.caf9a8f0.chunk.js"
+  },
+  {
+    "revision": "4e957751be355006b7d2",
+    "url": "/suryareactproject123/static/js/runtime-main.f9ab4302.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/suryareactproject123/static/media/logo.5d5d9eef.svg"
+  }
+]);
